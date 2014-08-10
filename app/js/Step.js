13feae9b1f7ab65bb3aa08c7386abd23d7a09386
Blair Caple <blair@kaaterskil.com>
2014-08-10 01:46:10 -0400

    Refactor question and response to Context and Strategy II

--- app/js/Step.js
+++ app/js/Step.js
@@ -1,51 +1,45 @@
-function Step(args){
-  var _data = args || {};
+var AppSteps = (function(that){
+  that.Step = Step;
 
-  this.get = function(prop){
-    if(typeof prop !== 'undefined') {
-      if(typeof _data[prop] != 'undefined') {
+  function Step(){
+    var _data = {};
+
+    this.get = function(prop){
+      if(typeof prop !== 'undefined') {
         return _data[prop];
       }
+      return undefined;
+    };
+
+    this.set = function(prop, newValue){
+      if(typeof prop !== 'undefined') {
+        var oldValue = _data[prop];
+        _data[prop] = newValue;
+        return oldValue;
+      }
+      return undefined;
+    };
+  }
+
+  Step.prototype = {
+    question : function(text){
+      if(typeof text !== 'undefined') {
+        return this.set('question', text);
+      }
+      return this.get('question');
+    },
+
+    responses : function(array){
+      if(typeof array !== 'undefined') {
+        return this.set('responses', array);
+      }
+      return this.get('responses');
+    },
+
+    response : function(response){
+
     }
-    return undefined;
   };
 
-  this.set = function(prop, value){
-    if(typeof prop !== 'undefined') {
-      var oldValue = _data[prop];
-      _data[prop] = value;
-      return oldValue;
-    }
-    return undefined;
-  };
-}
-
-Step.prototype = {
-  previous : function(value) {
-    if(typeof value !== 'undefined') {
-      return this.get('previous');
-    }
-    return this.set('previous', value);
-  },
-
-  next : function(value) {
-    if(typeof value !== 'undefined') {
-      return this.get('next');
-    }
-    return this.set('next', value);
-  },
-
-  title : function(value) {
-    if(typeof value !== 'undefined') {
-      return this.get('title');
-    }
-    return this.set('title', value);
-  },
-
-  description : function(value) {
-    if(typeof value !== 'undefined') {
-      return this.get('description');
-    }
-    return this.set('description', value);
-  },
-};
+  return that;
+})(AppSteps);
