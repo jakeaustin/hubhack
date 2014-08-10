dc648dac9d810a8a6675b6afc2bd6ea711f16f69
Blair Caple <blair@kaaterskil.com>
2014-08-10 01:44:24 -0400

    Refactor question and response to Context and Strategy

---
+++ app/js/Response.js
@@ -0,0 +1,42 @@
+var AppResponses = (function(that){
+  that.Response = Response;
+
+  function Response(){
+    var _data = {};
+
+    this.get = function(prop){
+      if(typeof prop !== 'undefined') {
+        return _data[prop];
+      }
+      return undefined;
+    };
+
+    this.set = function(prop, newValue){
+      if(typeof prop !== 'undefined') {
+        var oldValue = _data[prop];
+        _data[prop] = newValue;
+        return oldValue;
+      }
+      return undefined;
+    };
+  }
+
+  Response.prototype = {
+    responseText : function(text){
+      if(typeof text !== 'undefined') {
+        return this.set('responseText', text);
+      }
+      return this.get('responseText');
+    },
+
+    setNextStep : function(nextStep){
+      this.set('nextStep', nextStep);
+    },
+
+    getNextStep : function(){
+      return this.get('nextStep');
+    }
+  };
+
+  return that;
+})(AppResponses);
